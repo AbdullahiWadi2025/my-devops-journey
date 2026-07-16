6c669998d5283fa5760b0d120efa7c47cdf9323e
abdullahiWadi2025 <abdulahiyerow@gmail.com>
2026-07-16 09:52:42 -0700

    Add Flask Redis Docker Compose project

---
+++ 05-docker/projects/redis-flask-app/counter.py
@@ -0,0 +1,14 @@
+import os
+import redis
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(
+    host=redis_host,
+    port=redis_port,
+    decode_responses=True
+)
+
+def increment_visits():
+    return r.incr("visits")
